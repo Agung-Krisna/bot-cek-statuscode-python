c44143dc997ef35eb8dd97530681e74ae89649e0
gung <71138589+agungsoboru@users.noreply.github.com>
2022-12-11 06:26:34 +0800

    Update screenshot_api.py

--- screenshot_api.py
+++ screenshot_api.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse, RedirectResponse
 import os
 import datetime
+import os.path
 app = FastAPI()
 
 filename = ""
@@ -15,10 +16,28 @@ async def screenshot(domain : str, response_class=FileResponse):
     filename = f"{domain}{used_time}.jpg"
     os.system(f'wkhtmltoimage {domain} {filename}')
     
-    response = RedirectResponse(url=f"/getfile/{filename}")
+   # response = RedirectResponse(url=f"/getfile/{filename}")
     #return response
     return {"filename": filename}
 
 @app.get('/getfile/{filename}')
 async def getfile(filename: str, response_class=FileResponse):
     return FileResponse(f'{filename}')
+   #  try:
+           # file_exists = os.path.exists(filename)
+           # print(file_exists)
+           # if true:
+            
+            #return FileResponse(f'{filename}')
+            #file_exists = exists(path_to_file)
+                #while contents := file.file.read(1024 * 1024):
+                 #   f.write(contents)
+                #    else:
+                     #   return {"message": "There was an error get file"}
+                    
+        #except Exception:
+            #return {"message": "There was an error get file"}
+        #finally:
+         ##   file.file.close()
+
+      #  return {"message": f"Successfully uploaded {file.filename}"}
